6724219e199a6653d8457d99d9c41ecf5b4cd9d3
Danilo G. Veraszto <garbini@gmail.com>
2023-10-18 17:10:50 -0300

    Automated open of base projects

--- base_tmux.sh
+++ base_tmux.sh
@@ -2,35 +2,50 @@
 export PULL_MY_NON_PUBLIC_SCRIPT="$HOME/git/my-bash/my-custom-bashrc.sh"
 source $HOME/git/bash/my-custom-bashrc.sh
 unset GNOME_TERMINAL_SCREEN
-sessionName=myTmuxSession
-sessionGroup=myTmuxGroup
-tmux -S $MY_TMUX_SOCKET has-session -t $sessionName &> /dev/null
-hasSession=$?
-base='tmux -S $MY_TMUX_SOCKET'
-selectLayoutTiled="select-layout tiled"
-splitw="splitw -l5 \; select-pane -t -1"
-if test $hasSession -gt 0; then
-	andThen='-f $MY_TMUX_CONF new-session -s '"$sessionName"' -t '"$sessionGroup"' \; '\
-"$splitw"' \; '"$splitw"' \; '"$splitw"' \; splitw "journalctl --follow" \; '\
-'swap-pane -t4 \; '\
-'splitw "TERM=xterm-256color htop" \; select-layout tiled \; '\
-'swap-pane -t5 \; '\
-'select-pane -t0 \; '\
-'new-window "vim" \; '\
-'new-window "vim" \; '\
-'new-window \; '\
-'select-window -t 0'
-	built="$base $andThen"
-else
-	andThen='new-session -t '"$sessionGroup"
-	built="$base $andThen"
-fi
-echo $built
-gnome-terminal --full-screen -- /bin/bash -c "$built"
+#sessionName=myTmuxSession
+#sessionGroup=myTmuxGroup
+#tmux -S $MY_TMUX_SOCKET has-session -t $sessionName &> /dev/null
+#hasSession=$?
+#selectLayoutTiled="select-layout tiled"
+#splitw="splitw -l5 \; select-pane -t -1"
 
-tmuxInside="$MY_VIM_LOADERS_DIR_BASE/tmux.inside.vim"
+basic='tmux -S $MY_TMUX_SOCKET -f $MY_TMUX_CONF new-session'
+reports='"journalctl --follow" \; splitw "TERM=xterm-256color htop" \; select-layout even-horizontal'
+
+#if test $hasSession -gt 0; then
+#	andThen='-f $MY_TMUX_CONF new-session -s '"$sessionName"' -t '"$sessionGroup"' \; '\
+#"$splitw"' \; '"$splitw"' \; '"$splitw"' \; splitw "journalctl --follow" \; '\
+#'swap-pane -t4 \; '\
+#'splitw "TERM=xterm-256color htop" \; select-layout tiled \; '\
+#'swap-pane -t5 \; '\
+#'select-pane -t0 \; '\
+#'new-window "vim" \; '\
+#'new-window "vim" \; '\
+#'new-window \; '\
+#'select-window -t 0'
+#	built="$base $andThen"
+#else
+#	andThen='new-session -t '"$sessionGroup"
+#	built="$base $andThen"
+#fi
+#echo $built
+
+geometry="270x70"
+gnome-terminal --full-screen -- /bin/bash -c "$basic $reports"
+gnome-terminal --title "Bash" --geometry=$geometry -- /bin/bash -c "$basic"
+
+
+gnome-terminal --title "Vim" --geometry=$geometry -- /bin/bash -c "$basic \
+vim \"+cd $HOME\" \"+source $MY_DANVIM_DIR/basic_loader.vim\" \; \
+new-window vim \"+cd $MY_DANVIM_DIR\" \"+source $MY_DANVIM_DIR/basic_loader.vim\""
+
+
+gnome-terminal --title "Cloud Virtual Machine" --geometry=$geometry
+gnome-terminal --title "Launcher"
+
+#tmuxInside="$MY_VIM_LOADERS_DIR_BASE/tmux.inside.vim"
 #gnomeProject="$MY_PUBLIC_BASH_DIR/build_workspace_file_from_dir.sh && gnome-terminal --full-screen -- vim -S $tmuxInside"
-gnomeProject="gnome-terminal --full-screen -- vim -S $tmuxInside"
+#gnomeProject="gnome-terminal --full-screen -- vim -S $tmuxInside"
 #cd $MY_JAVA_DIR && eval $gnomeProject
 #cd $MY_JS_DIR && eval $gnomeProject
 #gnome-terminal --full-screen
